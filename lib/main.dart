18e970bf1de478289103733cfaeffbf65e043e6a
Paulo Valente Maia <paulovalentemaia@gmail.com>
2021-06-23 09:52:21 -0300

    Adicionando Appbar

--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,17 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
       home: Scaffold(
+    appBar: AppBar(
+      title: Text("Meu Primeiro App"),
+    ),
     body: Container(
       color: Colors.purple,
-      child: Container(
-        height: 200,
-        width: 200,
-        color: Colors.green,
+      child: Center(
+        child: Container(
+          height: 200,
+          width: 200,
+          color: Colors.green,
+        ),
       ),
     ),
   )));
