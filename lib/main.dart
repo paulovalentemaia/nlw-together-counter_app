22801cab33244da6e1c31afdcb61b6c4494fec43
Paulo Valente Maia <paulovalentemaia@gmail.com>
2021-06-23 09:19:25 -0300

    Adicionando child-novo widget

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,9 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: Container(color: Colors.green.shade900),
+    home: Container(
+      color: Colors.green.shade900,
+      child: ,
+      ),
   ));
 }
