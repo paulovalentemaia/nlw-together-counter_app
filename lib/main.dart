589c09302ebd0d71de45470849c1833180ef4d7e
Paulo Valente Maia <paulovalentemaia@gmail.com>
2021-06-23 12:41:43 -0300

    Add função increment

--- lib/main.dart
+++ lib/main.dart
@@ -2,16 +2,30 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    theme: ThemeData(primarySwatch: Colors.green),
-    home: Scaffold(
+      theme: ThemeData(primarySwatch: Colors.green), home: HomePage()));
+}
+
+class HomePage extends StatelessWidget {
+  var count = 0;
+
+  void increment() {
+    count++;
+    print(count);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
       appBar: AppBar(
         title: Text("Meu Primeiro App"),
       ),
       body: Center(child: Text("Contador\n0", textAlign: TextAlign.center)),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
-        onPressed: () {},
+        onPressed: () {
+          increment();
+        },
       ),
-    ),
-  ));
+    );
+  }
 }
