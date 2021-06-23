77db198ac1594889fc99a3576b44efd98305771c
Paulo Valente Maia <paulovalentemaia@gmail.com>
2021-06-23 10:06:42 -0300

    Add Tema

--- lib/main.dart
+++ lib/main.dart
@@ -2,19 +2,22 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-      home: Scaffold(
-    appBar: AppBar(
-      title: Text("Meu Primeiro App"),
-    ),
-    body: Container(
-      color: Colors.purple,
-      child: Center(
-        child: Container(
-          height: 200,
-          width: 200,
-          color: Colors.green,
-        ),
+      theme: ThemeData(
+        primarySwatch: Colors.green,
       ),
-    ),
-  )));
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Meu Primeiro App"),
+        ),
+        body: Container(
+          color: Colors.purple,
+          child: Center(
+            child: Container(
+              height: 200,
+              width: 200,
+              color: Colors.green,
+            ),
+          ),
+        ),
+      )));
 }
